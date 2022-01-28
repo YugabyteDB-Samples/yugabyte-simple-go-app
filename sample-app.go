52d9cb365ce15db8ff74c59b44bd60914c130600
Denis Magda <dmagda@gridgain.com>
2022-01-28 16:06:20 -0500

    basic impl

--- sample-app.go
+++ sample-app.go
@@ -9,13 +9,13 @@ import (
 )
 
 const (
-	host        = "2e82c651-1d80-47f2-8cdf-07612b4fbda8.aws.ybdb.io"
+	host        = ""
 	port        = 5433
 	dbName      = "yugabyte"
-	dbUser      = "admin"
-	dbPassword  = "DDkxo5ZhxzPgGTdxYrGfq7Ib-FRI0o"
-	sslMode     = "verify-full"
-	sslRootCert = "/Users/dmagda/Downloads/yb_cloud/root.crt"
+	dbUser      = ""
+	dbPassword  = ""
+	sslMode     = ""
+	sslRootCert = ""
 )
 
 func main() {
@@ -27,25 +27,58 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Read from the table.
-	var return_val int
-	rows, err := db.Query(`SELECT 1`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-	fmt.Printf("Query returned: ")
-	for rows.Next() {
-		err := rows.Scan(&return_val)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("Row[%d]\n", return_val)
-	}
-	err = rows.Err()
+	fmt.Println(">>>> Successfully connected to YugabyteDB!")
+
+	createDatabase(db)
+	selectAccounts(db)
+
+	defer db.Close()
+}
+
+func createDatabase(db *sql.DB) {
+	stmt := `CREATE TABLE IF NOT EXISTS DemoAccount (
+						id int PRIMARY KEY,
+						name varchar,
+						age int,
+						country varchar,
+						balance int)`
+
+	_, err := db.Exec(stmt)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	defer db.Close()
+	stmt = `INSERT INTO DemoAccount VALUES 
+				(1, 'Jessica', 28, 'USA', 10000),
+				(2, 'John', 28, 'Canada', 9000)`
+
+	_, err = db.Exec(stmt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(">>>> Successfully created DemoAccount table.")
+}
+
+func selectAccounts(db *sql.DB) {
+	fmt.Println(">>>> Selecting accounts:")
+
+	rows, err := db.Query("SELECT name, age, country, balance FROM DemoAccount")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var name, country string
+	var age, balance int
+
+	for rows.Next() {
+		err = rows.Scan(&name, &age, &country, &balance)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("name = %s, age = %v, country = %s, balance = %v\n", name, age, country, balance)
+	}
+
+	defer rows.Close()
 }
